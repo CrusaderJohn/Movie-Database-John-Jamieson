f24f0ded9b0397a7b95115790d9a45919631b44d
John Jamieson <jamieson.john@gmail.com>
2023-01-05 21:31:45 -0500

    add-movie added

--- server.js
+++ server.js
@@ -15,16 +15,42 @@ const db = mysql.createConnection(
         host: 'localhost',
         // MySQL username,
         user: 'root',
-        // TODO: Add MySQL password
         password: '7Bbz8nJ9vUj-',
-        database: 'books_db'
+        database: 'movie_db'
     },
-    console.log(`Connected to the books_db database.`)
+    console.log(`Connected to the database.`)
 );
 
+
+
+app.get('/api/movies', (req, res) => {
+    db.query('SELECT * FROM movies', function (error, results) {
+        res.json(results);
+    });
+});
+
+// POST request for reviews
+app.post('/api/add-movie', (req, res) => {
+    // Inform the client that their POST request was received
+    res.json(`${req.method} request received to add a review`);
+    // Log our request to the terminal
+    console.info(`${req.method} request received to add a review`);
+    db.query('INSERT INTO movies (movie_name, movie_year) VALUES (?, ?)', [req.body.movie_name, req.body.movie_year], function (error, results) {
+        res.json(results);
+    });
+
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 // Query database
 
-let deletedRow = 2;
+/*let deletedRow = 2;
 
 db.query(`DELETE FROM favorite_books WHERE id = ?`, deletedRow, (err, result) => {
     if (err) {
@@ -49,4 +75,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+});*/
